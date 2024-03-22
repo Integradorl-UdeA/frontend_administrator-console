29533ac9a70b4f7e7cc516d9464f31923f212956
David Upegui <dupegui2002@gmail.com>
2024-03-22 14:27:34 -0500

    Styles applied to LoginImage

--- components/Login/LoginImage.tsx
+++ components/Login/LoginImage.tsx
@@ -3,7 +3,12 @@ import styles from '@/styles/Login/Login.module.css';
 
 function LoginImage() {
   return (
-    <div className={styles.sideImgContainer}>LoginImage</div>
+    <div className={styles.sideImgContainer}>
+      
+      <div className={styles.sistemasLogoContainer}>
+        <img className={styles.sistemasLogo} src="/images/ig-sistemas.png" alt="" />
+      </div>
+    </div>
   )
 }
 
