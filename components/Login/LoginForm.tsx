aa28b1304bdd954e4713c2fe7bdad398503dcb9b
David Upegui <dupegui2002@gmail.com>
2024-03-30 00:48:34 -0500

    The authentication functionality has been completed
    - The authentication can be made from de /login instead of the NextAuth
      login provider
    - TO FIX:
      - Typescript errors in the onSubmit function of the login form
      - Manage the arraving of the user data (the POST petition for login
        must return the user information and the token)

--- components/Login/LoginForm.tsx
+++ components/Login/LoginForm.tsx
@@ -1,28 +1,46 @@
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
 import { FaUser } from 'react-icons/fa';
 import InputPassword from '../common/InputPassword';
 import styles from '@/styles/Login/Login.module.css';
 import commonStyles from '@/styles/common/Inputs.module.css';
-import { useForm } from 'react-hook-form';
+import { FieldValue, FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
 
 function LoginForm() {
+	const router = useRouter();
+	const [errors, setErrors] = useState([])
 	const { register, handleSubmit } = useForm()
+	const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+		const responseNextAuth = await signIn("credentials", {
+			username: data.username,
+			password: data.password,
+			redirect: false,
+		});
+		if (responseNextAuth?.error) {
+			setErrors(responseNextAuth?.error);
+			return;
+		}
+		router.push("/")
+	}
 	return (
 		<section className={styles.formContainer}>
-				<span className={styles.userIconContainer}>
-					<FaUser className={styles.userIcon}/>
-				</span>
+			<span className={styles.userIconContainer}>
+				<FaUser className={styles.userIcon} />
+			</span>
 			<span className={styles.welcome}>¡Bienvenid@!</span>
 			<span className={styles.label}>Inicia sesión o registrate</span>
 
-			<form className={styles.form} onSubmit={handleSubmit(data => console.log(data))}>
+			<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
 				<div className={styles.formSection}>
-					<input className={commonStyles.inputText}type='text' {...register('username')} placeholder='Username' />
+					<input className={commonStyles.inputText} type='text' {...register('username')} placeholder='Username' />
 				</div>
 				<div className={styles.formSection}>
 					<InputPassword register={register}></InputPassword>
 				</div>
+				<span>Error: {errors}</span>
 				<button className={commonStyles.btnSubmit} type='submit'>Ingresar</button>
 			</form>
 		</section>
