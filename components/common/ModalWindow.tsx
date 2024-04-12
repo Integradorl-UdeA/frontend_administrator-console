ce15102a1f59c066043205e55fb8f648978606b6
David Upegui <dupegui2002@gmail.com>
2024-04-12 11:50:05 -0500

    Modal window with all the styles applied correctly

--- components/common/ModalWindow.tsx
+++ components/common/ModalWindow.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import styles from '@/styles/modals/Modal.module.css';
+import { IoClose } from "react-icons/io5";
 
-interface Props{
-    children: React.ReactNode,
-    title: string
-    close: () => void
+
+interface Props {
+  children: React.ReactNode,
+  title: string
+  close: () => void
 }
-const ModalWindow = ( {children, title, close}: Props ) => {
+const ModalWindow = ({ children, title, close }: Props) => {
   return (
 
     <div className={styles.overlay}>
       <div className={styles.container}>
         <div className={styles.header}>
           <h3 className={styles.title}>{title}</h3>
-          <button onClick={close} className={styles.closeButton}>x</button>
+          <button onClick={close} className={styles.closeButton}>
+            <IoClose></IoClose>
+          </button>
         </div>
         <div className={styles.body}>
           {children}
