b16271fabccc8916aed842fe0d6a0b8b45bc15a1
Laura Tascón <laura.tascon@udea.edu.co>
2024-04-18 10:35:59 -0500

    something

--- app/(main)/inventario/page.tsx
+++ app/(main)/inventario/page.tsx
@@ -2,7 +2,6 @@ import React from 'next';
 import TableInventory from '@/components/common/TableInventory';
 import { AddButton } from '@/components/common/table/AddButton';
 import SearchInput from '@/components/common/table/SearchInput';
-import FilterOptions from '@/components/common/table/FilterOptions';
 import { HeaderInfoTablePage } from '@/components/common/table/HeaderInfoTablePage';
 const options = ['Todos', 'Disponible', 'No disponible'];
 
@@ -22,10 +21,22 @@ const InventoryPage = () => {
 					</div>
 				</div>
 				<div className='mt-6 md:flex md:items-center md:justify-between'>
-					<FilterOptions options={options}></FilterOptions>
+					<div className="inline-flex overflow-hidden bg-white border divide-x rounded-lg dark:bg-gray-900 rtl:flex-row-reverse dark:border-gray-700 dark:divide-gray-700">
+						<button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 bg-gray-100 sm:text-sm dark:bg-gray-800 dark:text-gray-300">
+							Ver todo
+						</button>
+
+						<button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
+							Disponible
+						</button>
+
+						<button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
+							No disponible
+						</button>
+					</div>
 					<SearchInput></SearchInput>
 				</div>
-				<TableInventory/>
+				<TableInventory />
 			</section>
 		</div>
 	);
