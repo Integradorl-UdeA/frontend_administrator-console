902b941c3d56fc0d9fde5dde479302e08350476d
David Upegui <dupegui2002@gmail.com>
2024-04-24 21:33:51 -0500

    Close button in the Category Form and some hover effects

--- components/forms/CategoryForm/CategoryForm.tsx
+++ components/forms/CategoryForm/CategoryForm.tsx
@@ -7,6 +7,8 @@ import type { FieldValues, SubmitHandler } from 'react-hook-form';
 import AdditionalCatAttributes from './AdditionalCatAttributes';
 import { useCategoryForm } from '@/store/categoryFormStore';
 import { FaPlus } from 'react-icons/fa6';
+import { AiOutlineClose } from "react-icons/ai";
+
 
 interface Props{
 	closeModal?: () => void
@@ -59,9 +61,17 @@ const CategoryForm = ({closeModal}: Props) => {
 				</div>
 				<AdditionalCatAttributes control={control} />
 				{fieldFormStatus === 0 && (
-					<div className='flex mt-8'>
+					<div className=' flex justify-between mt-8'>
 						<button
-							className=' flex items-center justify-center bg-white w-max h-max py-2 px-3  ml-auto rounded-full font-medium shadow-lg justify-self-end'
+							className=' flex items-center justify-center bg-white w-max h-max py-2 px-3 rounded-full font-medium shadow-lg hover:bg-red-100'
+							type='button'
+							onClick= {() => {(closeModal != null) && closeModal()}}
+						>
+							<AiOutlineClose className='text-white bg-red-500 rounded-full p-1 mr-2 text-2xl' />
+							Cancelar
+						</button>
+						<button
+							className=' flex items-center justify-center bg-white py-2 px-3 rounded-full font-medium shadow-lg hover:bg-greenThree/10'
 							type='submit'
 						>
 							<FaPlus className='text-white bg-greenThree rounded-full p-1 mr-2 text-2xl' />
