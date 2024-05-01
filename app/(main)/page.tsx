c03c8be988cacdea37311865957fe13efc011ed9
David Upegui <dupegui2002@gmail.com>
2024-04-30 23:46:11 -0500

    Installed: jwt-decode

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,23 +1,11 @@
 'use client'
-import ModalWindow from '@/components/common/ModalWindow';
-import CategoryForm from '@/components/forms/CategoryForm/CategoryForm';
-import React, { useState } from 'react';
+import React from 'react';
 
 const Home = () => {
-	const [modal, setModal] = useState(true)
-	const closeModal = () => { setModal(false) }
-	const openModal = () => { setModal(true) }
+
 	return (
 		<main>
-			{
-				modal && (
-					<ModalWindow title='Crear nueva categoría' close={closeModal} widthClass='w-fit'>
-						<CategoryForm closeModal={closeModal}/>
-					</ModalWindow>
-				)
-			}
-			<button onClick={openModal}>Open Modal</button>
-			<h1>algo</h1>
+			
 		</main>
 	);
 };
