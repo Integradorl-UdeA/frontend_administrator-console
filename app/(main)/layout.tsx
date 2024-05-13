0cdb197c21010ecae9cc4204c9c11f8e78673990
David Upegui <dupegui2002@gmail.com>
2024-05-13 18:06:14 -0500

    React-query initial config in app/(main)/layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,32 +1,35 @@
 /* eslint-disable react/react-in-jsx-scope */
 'use client';
 import '@/styles/globals.css';
-import React from 'react'
+import React from 'react';
 import { Inter } from 'next/font/google';
 import { SideNav } from '@/components/Layout/SideNav/SideNav';
 import { TopBar } from '@/components/Layout/topBar/TopBar';
 import layoutStyles from '@/styles/Layout.module.css';
 import SessionAuthProvider from '@/context/SessionAuthProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const inter = Inter({ subsets: ['latin'] });
 
 interface RootLayoutProps {
 	children: React.ReactNode;
 }
-
+const queryClient = new QueryClient();
 const RootLayout = ({ children }: RootLayoutProps) => {
 	return (
 		<html lang='en'>
 			<head>
 				<title>Consola de Administrador</title>
 			</head>
-			<body className={`${inter.className} ${layoutStyles.bodyContainer}`}>
-				<SessionAuthProvider>
-					<SideNav />
-					<TopBar />
-					<main className={layoutStyles.mainContainer}>{children}</main>
-				</SessionAuthProvider>
-			</body>
+			<QueryClientProvider client={queryClient}>
+				<body className={`${inter.className} ${layoutStyles.bodyContainer}`}>
+					<SessionAuthProvider>
+						<SideNav />
+						<TopBar />
+						<main className={layoutStyles.mainContainer}>{children}</main>
+					</SessionAuthProvider>
+				</body>
+			</QueryClientProvider>
 		</html>
 	);
 };
