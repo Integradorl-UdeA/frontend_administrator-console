fda2837b493024246901be8bfffaa602472d3fb4
superpollo2 <laura.tascon@udea.edu.co>
2024-05-13 18:21:27 -0500

    avatar whit contentful

--- components/Layout/topBar/TopBar.tsx
+++ components/Layout/topBar/TopBar.tsx
@@ -3,13 +3,18 @@ import { Inter } from 'next/font/google';
 import React from 'react';
 import styles from '@/styles/TopBar.module.css';
 import { GoSignOut } from 'react-icons/go';
-import { signOut , useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
+import { useGetContentful } from '@/hooks/useGetContentful';
+import { Spinner } from '@/components/common/Spinner';
 
 
 const inter = Inter({ subsets: ['latin'] });
 
 const TopBar = () => {
 	const userInfo = useSession().data?.user;
+	const {data, isLoading}  = useGetContentful(userInfo?.username);
+	 // Empty dependency array to run only once on mount
+
 	const role =
 		userInfo?.role === 'AUXADMI' ? 'Auxiliar Administrativo' : 'Jefe';
 	return (
@@ -19,7 +24,10 @@ const TopBar = () => {
 				<span>{role}</span>
 			</div>
 			<div>
-				<img src='/images/avatar.png' alt='' className={styles.img} />
+			{isLoading && (
+				<Spinner color={'gray-300'} />
+			)}
+				<img src={data?.fields.photo.fields.file.url} alt='' className={styles.img} />
 			</div>
 			<button
 				onClick={async () => {
