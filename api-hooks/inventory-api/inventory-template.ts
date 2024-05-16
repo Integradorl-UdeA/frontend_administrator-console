df2519c4a129154265f842a853e2806f332f0600
David Upegui <dupegui2002@gmail.com>
2024-05-16 15:13:07 -0500

    Api calls now consuming the BACKEND_URL environment variable

--- api-hooks/inventory-api/inventory-template.ts
+++ api-hooks/inventory-api/inventory-template.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getInventoryTemplate= (token:string) => {
     const categoryTemplate = axios.create({
-        baseURL: 'http://192.168.30.66:8081/api/console-lis/user/inventory/item',
+        baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/user/inventory/item`,
         headers: {
             Authorization: `Bearer ${token}`
         }
