e8922f88563f3cfed7ed9e26a2260889e097d91d
David Upegui <dupegui2002@gmail.com>
2024-05-17 13:39:40 -0500

    LoanForm now in a modal

--- app/(main)/prestamo/page.tsx
+++ app/(main)/prestamo/page.tsx
@@ -5,10 +5,22 @@ import { AddButton } from '@/components/common/table/AddButton';
 import SearchInput from '@/components/common/table/SearchInput';
 import FilterOptions from '@/components/common/table/FilterOptions';
 import { HeaderInfoTablePage } from '@/components/common/table/HeaderInfoTablePage';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
+import ModalWindow from '@/components/common/ModalWindow';
+import LoanFormProvider from '@/components/forms/loan-form/LoanFormProvider';
+import LoanForm from '@/components/forms/loan-form/LoanForm';
 const options = ['Todos', 'Activo', 'Vencido', 'Devuelto'];
 
 const LoanPage = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const openModal = () => {
+		setIsOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsOpen(false);
+	};
+
 	return (
 		<div>
 			<section className='container px-4 mx-auto'>
@@ -23,7 +35,7 @@ const LoanPage = () => {
 						description='Estos préstamos se han realizado en los últimos 6 meses.'
 					></HeaderInfoTablePage>
 					<div className='flex item s-center mt-4 gap-x-3'>
-						<AddButton onClick={() => {}} text={'Añadir préstamo'}></AddButton>
+						<AddButton onClick={() => {openModal()}} text={'Añadir préstamo'}></AddButton>
 					</div>
 				</div>
 				<div className='mt-6 md:flex md:items-center md:justify-between'>
@@ -33,6 +45,13 @@ const LoanPage = () => {
 				<Suspense fallback={<p>Cargando Tabla...</p>}>
 					<TableLoan />
 				</Suspense>
+				{isOpen && (
+					<LoanFormProvider>
+						<ModalWindow close={closeModal} title='Realizar un préstamo'>
+							<LoanForm />
+						</ModalWindow>
+					</LoanFormProvider>
+				)}
 			</section>
 		</div>
 	);
---
+++ components/forms/loan-form/LoanForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoanForm = () => {
+  return (
+    <form>
+        Pruebitaaaa
+    </form>
+  )
+}
+
+export default LoanForm
