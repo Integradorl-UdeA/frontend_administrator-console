1969fca9d99f5f1248a18fa2cf3765d179613daa
David Upegui <dupegui2002@gmail.com>
2024-05-24 09:46:01 -0500

    Only the lendable items are rendered in the selection table
    - Refactor: LoanTableRow -> ItemSelectionTableRow
    - It was necessary to call the items and the categories cause using the
      items table was insufficient

--- components/forms/loan-form/LendedItemSelection.tsx
+++ components/forms/loan-form/LendedItemSelection.tsx
@@ -1,17 +1,19 @@
 import { useLoanForm } from '@/store/loan-form-store';
-import React, { useEffect } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import btnStyles from '@/styles/common/button-styles.module.css';
 import { TiTick } from 'react-icons/ti';
 import { IoMdArrowRoundBack } from 'react-icons/io';
 import { useModalContext } from '@/components/common/ModalWindow/modal-window-context';
 import { Table } from '@/components/common/table/Table';
-import { getItemsPerPage } from '@/api-hooks/inventory-api/getItemsPerPage';
-import { useSession } from 'next-auth/react';
-import { LoanTableRow } from './LoanTableRow';
+import { ItemSelectionTableRow } from './ItemSelectionTableRow';
 import { useFormContext } from 'react-hook-form';
+import { getAllItems } from '@/api-hooks/inventory-api/getAllItemsQuery';
+import { getSessionToken } from '@/api-hooks/getSessionToken';
+import { getAllCategories } from '@/api-hooks/category-api/getAllCategoriesQuery';
+import type { ICategory } from '@/types/categoryTypes';
 
 const LendedItemSelection = () => {
-	const token = useSession().data?.token?.token;
+	const token = getSessionToken();
 	const { setModalTitle, setModalWidthClass } = useModalContext();
 	const { setValue } = useFormContext();
 	const setFormSection = useLoanForm((state) => state.setFormSection);
@@ -19,7 +21,11 @@ const LendedItemSelection = () => {
 	const setSelectedItemId = useLoanForm((state) => state.setSelectedItemId);
 
 	const tableHeaders = ['ID', 'Categoría', 'Atributos'];
-	const { data: itemsPage } = getItemsPerPage(token as string, 0);
+	const {data: items} = getAllItems(token)
+	const {data: categories} = getAllCategories(token)
+
+	const lendableItems = items?.filter( item => item.lendable)
+	console.log("Lendable Items: ", lendableItems)
 
 	const handleSelectItem = () => {
 		setValue('itemId', selectedItemId);
@@ -38,12 +44,15 @@ const LendedItemSelection = () => {
 					<div className='inline-block min-w-full py-2 align-middle md:px-6 lg:px-8'>
 						<div className='overflow-hidden border border-gray-200 md:rounded-lg'>
 							<Table column={tableHeaders}>
-								{itemsPage?.items?.map((itemTable) => {
+								{lendableItems?.map((item) => {
+									const category = categories?.filter(cat => item.categoryId === cat.id)[0]
 									return (
-										<LoanTableRow
-											key={itemTable.id}
-											itemTable={itemTable}
-										></LoanTableRow>
+										<Suspense key={item.itemId}>
+											<ItemSelectionTableRow
+												category={category as ICategory}
+												item={item}
+											></ItemSelectionTableRow>
+										</Suspense>
 									);
 								})}
 							</Table>
